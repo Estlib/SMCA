820236364df8d95bbd0fff268ffa371315381638
Estlib <75094683+Estlib@users.noreply.github.com>
2026-07-21 23:59:05 +0300

    postscontroller contents to be added

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -8,5 +8,34 @@ namespace SMCA.Controllers
 {
     class PostsController
     {
+        /* basic db operations */
+
+        //get-all
+
+        //get-id
+
+        //get-filtered
+
+        //get-timerange
+
+        //create
+
+        //create-postnow
+
+        //create-draft
+
+        //update
+
+        //delete
+
+        /*quick action related*/
+
+        //create-duplicate
+
+        //create-repost
+
+        //update-postingtime
+
+        //update-draft
     }
 }
